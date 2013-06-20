356211ada40002a7965d72743daacede4cdf8d42
sahusoft <sahumail@126.com>
2013-06-20 22:44:42 +0800

    添加：pthread_mutex和原子操作的对比

--- c_c++/gcc_sync_test/gcc_sync_test.c
+++ c_c++/gcc_sync_test/gcc_sync_test.c
@@ -1,35 +1,54 @@
 #include <stdio.h>
-#include <pthread.h>
 #include <stdlib.h>
+#include <pthread.h>
 
-static int count = 0;
+#define TEST_ROUND  20000
+#define THREAD_NUM  10
 
+#define SYNC
+#define LOCKLESS
+
+#ifndef LOCKLESS
+	pthread_mutex_t mutex_lock;
+#endif
+
+static volatile int count = 0;
 
 void *test_func(void *arg)
 {
-	int i=0;
-	for(i=0;i<20000;++i){
-		__sync_fetch_and_add(&count,2);
-	}
-	for(i=0;i<20000;++i) {
-		__sync_fetch_and_sub(&count,1);
+	int i = 0;
+	for(i = 0; i < TEST_ROUND; i++){
+#ifdef SYNC
+#ifdef LOCKLESS
+		__sync_fetch_and_add(&count, 1);
+#else
+		pthread_mutex_lock(&mutex_lock);
+		count++;
+		pthread_mutex_unlock(&mutex_lock);
+#endif
+#else
+		count++;
+#endif
 	}
 	return NULL;
 }
 
 int main(int argc, const char *argv[])
 {
-	pthread_t id[20];
+	pthread_t thread_ids[THREAD_NUM];
 	int i = 0;
 
-	for(i=0;i<20;++i){
-		pthread_create(&id[i],NULL,test_func,NULL);
+#ifndef LOCKLESS
+	pthread_mutex_init(&mutex_lock, NULL);
+#endif
+	for(i = 0; i < sizeof(thread_ids)/sizeof(pthread_t); i++){
+		pthread_create(&thread_ids[i], NULL, test_func, NULL);
 	}
 
-	for(i=0;i<20;++i){
-		pthread_join(id[i],NULL);
+	for(i = 0; i < sizeof(thread_ids)/sizeof(pthread_t); i++){
+		pthread_join(thread_ids[i], NULL);
 	}
 
-	printf("%d\n",count);
+	printf("count=%d\r\n", count);
 	return 0;
 }
